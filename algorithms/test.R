d93233127e518ebce8f099e2ac1339b9d88abc51
hhant-max <sofia.sun.apply@outlook.com>
2022-11-22 14:10:12 +0100

    julia

---
+++ algorithms/test.R
@@ -0,0 +1,7 @@
+# install.packages('/home/sfy/Documents/VScodeProject/Thesis/algorithms/cubt_3.2.tar.gz',repos = NULL,type = "source")
+library('cubt')
+
+installed.packages()
+install.packages('clue')
+install.packages('partitions')
+install.packages('gmp')
